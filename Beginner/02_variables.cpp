cac17a753ef7b37e991f3b59a36b3e7680b68a1e
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-02-24 11:55:44 +0200

    variables added

---
+++ Beginner/02_variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age = 30;
+    int double pi = 3.14159;
+    char initial = 'A';
+    string name = "Alice Smith";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "initial: " << initial << endl;
+    return 0;
+}
