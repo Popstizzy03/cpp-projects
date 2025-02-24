c5afb7d64b19a5d3cc0f7d81ba6a1f8f98d10f7d
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-02-24 11:33:37 +0200

    Hello, World

---
+++ Beginner/01_hello_world.cpp
@@ -0,0 +1,16 @@
+// Beginner/01_hello_world.cpp
+/*#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+    old version, this course will only use the latest C++ vesion*/
+
+    #include <iostream>
+    using namespace std;
+
+    int main() {
+        cout << "Hello, World" << endl;
+        return 0;
+    }
