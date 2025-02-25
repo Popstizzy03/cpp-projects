9a109d8e7f562853f5b91193ec9a809e337b0378
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-02-25 10:36:47 +0200

    for loop added

---
+++ Beginner/05_for_loop.cpp
@@ -0,0 +1,11 @@
+// Beginner/05_for_loop.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; ++i) {
+        cout << "Iteration: " << i << endl;
+    }
+    return 0;
+}
