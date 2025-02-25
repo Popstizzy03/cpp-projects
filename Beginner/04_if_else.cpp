1dcc3e60899b2539b867483ec94f9037a079fd55
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-02-25 10:33:52 +0200

    input output and if else added

---
+++ Beginner/04_if_else.cpp
@@ -0,0 +1,19 @@
+// Beginner/04_if_else.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number > 0) {
+        cout << "The number is positive" << endl;
+    } else if (number < 0) {
+        cout << "The number is negative" << endl;
+    } else {
+        cout << "The number is 0" << endl;
+    }
+    return 0;
+}
