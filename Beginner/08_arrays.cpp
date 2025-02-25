66194c5ae44803bc984db6ee72b694f411400b44
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-02-25 10:41:48 +0200

    arrays.cpp added

---
+++ Beginner/08_arrays.cpp
@@ -0,0 +1,13 @@
+// Beginner/08_arrays.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numbers[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; ++i) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
