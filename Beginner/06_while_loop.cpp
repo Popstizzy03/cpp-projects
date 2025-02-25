9476bb1a5c074edff81e40fb8e0fda1d20abd120
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-02-25 10:38:35 +0200

    while loop added

---
+++ Beginner/06_while_loop.cpp
@@ -0,0 +1,12 @@
+// Beginner/06_while_loop.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0;
+    while (count < 5) {
+        cout << "Count: " << count << endl;
+        count ++;
+    }
+    return 0;
+}
