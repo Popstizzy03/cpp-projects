4f3b853f6141eec7a2ab7e39310d06b612f16065
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-03-02 20:22:38 +0200

    leap year check

---
+++ Beginner/18_leap_year_check.cpp
@@ -0,0 +1,17 @@
+// Beginner/18_leap_year_check.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int year;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        cout << year << " is a leap year." << endl;
+    } else {
+        cout << year << " is not a leap year." << endl;
+    }
+    return 0;
+}
