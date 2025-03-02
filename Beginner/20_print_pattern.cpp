46ba837b0eb3bf3b2a495a12ed270f2213ee079c
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-03-02 20:24:29 +0200

    print pattern

---
+++ Beginner/20_print_pattern.cpp
@@ -0,0 +1,15 @@
+// Beginner/20_print_pattern.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int rows = 5;
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
