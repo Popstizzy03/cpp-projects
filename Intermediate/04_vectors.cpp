4c9964cceb84e3ae6fa2d338529da2902b181650
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-03-06 12:57:30 +0200

    04_vectors.cpp

---
+++ Intermediate/04_vectors.cpp
@@ -0,0 +1,16 @@
+// Intermediate/04_vectors.cpp
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    numbers.push_back(6);
+    for (int num : numbers) {
+        cout << num << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
