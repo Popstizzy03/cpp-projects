11ee5849c51c4c2397e2b6f7074cf4114ea043eb
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-03-10 21:49:45 +0200

    linked list

---
+++ Intermediate/09_linked_list.cpp
@@ -0,0 +1,14 @@
+// Intermediate/09_linked_list.cpp
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* head = nullptr;
+    //...
+    return 0;
+}
