66c52e3408668edabe2993629edf332f9396eba0
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-03-17 03:34:51 +0200

    next step: fix bugs

--- Advanced/02_coroutine_generator.cpp
+++ Advanced/02_coroutine_generator.cpp
@@ -2,29 +2,27 @@
 #include <iostream>
 #include <coroutine>
 
-using namespace std;
-
 struct Generator {
     struct promise_type {
         int current_value;
-        suspend_always yield_value(int value) {
+        std::suspend_always yield_value(int value) {
             current_value = value;
             return {};
         }
-        suspend_always initial_suspend() { return {}; }
-        suspend_always final_suspend() noexcept { return {}; }
-        Generator get_return_object() { return Generator{coroutine_handle<promise_type>::from_promise(*this)}; }
+        std::suspend_always initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
+        Generator get_return_object() { return Generator{std::coroutine_handle<promise_type>::from_promise(*this)}; }
         void unhandled_exception() {}
     };
-    coroutine_handle<promise_type> handle;
-    Generator(coroutine_handle<promise_type> h) : handle(h) {}
+    std::coroutine_handle<promise_type> handle;
+    Generator(std::coroutine_handle<promise_type> h) : handle(h) {}
     ~Generator() { if (handle) handle.destroy(); }
     int value() { return handle.promise().current_value; }
     bool next() { return handle.resume(), !handle.done(); }
 };
 
 Generator generateNumbers(int count) {
-    for (int i =0; i < count; ++i) {
+    for (int i = 0; i < count; ++i) {
         co_yield i;
     }
 }
@@ -32,8 +30,8 @@ Generator generateNumbers(int count) {
 int main() {
     auto generator = generateNumbers(5);
     while (generator.next()) {
-        cout << generator.value() << " ";
-        cout << endl;
-        return 0;
+        std::cout << generator.value() << " ";
     }
+    std::cout << std::endl;
+    return 0;
 }
