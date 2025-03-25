176ca5feb0e4628ad4e7df4866584180f14f5ae2
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-03-26 01:53:17 +0200

    Create 21_simple_area_calculator.cpp

---
+++ Beginner/21_simple_area_calculator.cpp
@@ -0,0 +1,14 @@
+// Beginner/21_simple_area_calculator.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+  double length, width;
+  cout << "Enter width: ";
+  cin >> width;
+  cout << "Enter length: ";
+  cin >> length;
+  cout << "Area " << width * length << endl;
+
+  return 0;
+}
